ea665f591f7f1eefb22c8b13df177ed0737ab0ca
sivarajagopalans <sivarajagopalans@gmail.com>
2024-07-05 13:16:58 +0530

    create slice

--- src/slices/tasksSlices.jsx
+++ src/slices/tasksSlices.jsx
@@ -13,8 +13,21 @@ const taskSlice = createSlice({
             let task = { ...action.payload, id };
             state.tasksList.push(task);
         },
+        removeTaskFromList: (state, action) => {
+            state.tasksList = state.tasksList.filter((task) => {
+                return task.id !== action.payload.id
+            })
+        },
+        updateTaskInList: (state, action) => {
+            state.tasksList = state.tasksList.map((task) => {
+                return task.id === action.payload.id ? action.payload.id : task;
+            })
+        },
+        setSeletedTask: (state, action) => {
+            return state.selectedTask = action.payload;
+        }
     }
 })
 
-export const{addTaskToList} =taskSlice.actions;
+export const { addTaskToList, removeTaskFromList, updateTaskInList, setSeletedTask } = taskSlice.actions;
 export default taskSlice.reducer;
